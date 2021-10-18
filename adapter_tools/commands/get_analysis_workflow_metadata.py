770602499e271cb9850bba1e5bef75c46546c182
Wes Dingman <wdingman@asymmetrik.com>
2021-10-18 10:50:11 -0400

    Move command line utils from pipeline-tools to adapter-tools

---
+++ adapter_tools/commands/get_analysis_workflow_metadata.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import google.auth
+import google.auth.transport.requests
+import re
+
+from adapter_tools.utilities.http_requests import HttpRequests
+
+
+def get_analysis_workflow_id(analysis_output_path):
+    """Parse the analysis workflow id from one of its output paths, and write the id to a file so that it is available
+    outside of the get_analysis task.
+
+    Args:
+        analysis_output_path (str): path to workflow output file.
+
+    Returns:
+        workflow_id (str): string giving Cromwell UUID of the workflow.
+    """
+    # Get the last match for UUID prior to the file name (in case the file is
+    # named with a UUID) to ensure it is the subworkflow id
+    url = analysis_output_path.rsplit('/', 1)[0]
+    uuid_regex = r"([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})"
+    workflow_id = re.findall(uuid_regex, url)[-1]
+    print('Got analysis workflow UUID: {0}'.format(workflow_id))
+    with open('workflow_id.txt', 'w') as f:
+        f.write(workflow_id)
+    return workflow_id
+
+
+def get_auth_headers():
+    """ Get a bearer token from the default google account credentials on the machine that executes
+    this function. The credentials must have the scopes "https://www.googleapis.com/auth/userinfo.email"
+    and "https://www.googleapis.com/auth/userinfo.profile", which Cromwell will add automatically if
+    it is confiugred to use the Pipelines API v2 backend.
+
+    Returns:
+        headers (dict): authorization header containing bearer token {'Authorization': 'bearer 12345'}
+    """
+    credentials, project = google.auth.default()
+    if not credentials.valid:
+        credentials.refresh(google.auth.transport.requests.Request())
+    headers = {}
+    credentials.apply(headers)
+    return headers
+
+
+def get_metadata(cromwell_url,
+                 workflow_id,
+                 http_requests,
+                 include_keys=[],
+                 include_subworkflows="False"
+):
+    """Get metadata for analysis workflow from Cromwell and write it to a JSON file. This is only
+    compatible with instances of Cromwell that use SAM for Identity Access Management (IAM), such
+    as Cromwell-as-a-Service.
+
+    Args:
+        cromwell_url (str): Url to the cromwell environment the workflow was run in.
+        workflow_id (str): The analysis workflow id.
+        http_requests: `http_requests.HttpRequests` instance, a wrapper around requests provides better retry and
+                       logging.
+
+    Raises:
+        requests.HTTPError: For 4xx errors or 5xx errors beyond the timeout
+    """
+
+    def log_before(workflow_id):
+        print('Getting metadata for workflow {}'.format(workflow_id))
+
+    headers = get_auth_headers()
+
+    # There is an issue with the current CromIAM that huge response content can
+    # break the gzip compressing and session, which will raise an error:
+    # `requests.exceptions.ChunkedEncodingError`. Check here:
+    # https://github.com/broadinstitute/cromwell/issues/4708 for the details.
+    # As a workaround, we need to pass `Accept-Encoding: identity` to the header
+    # to force disabling the compressing
+    headers['Accept-Encoding'] = 'identity'
+
+    base_url = cromwell_url.strip('/')
+    url = '{0}/api/workflows/v1/{1}/metadata?expandSubWorkflows={2}'.format(base_url,
+                                                                            workflow_id,
+                                                                            include_subworkflows.lower()
+    )
+
+    if include_keys:
+        print(f'Including keys: {", ".join(include_keys)}')
+        key_query = f'&includeKey={"&includeKey=".join(include_keys)}'
+        url += key_query
+
+    response = http_requests.get(url, headers=headers, before=log_before(workflow_id))
+    with open('metadata.json', 'w') as f:
+        json.dump(response.json(), f, indent=2)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--analysis_output_path', required=True)
+    parser.add_argument('--cromwell_url', required=True)
+    parser.add_argument('--include_keys', required=False, nargs='+')
+    parser.add_argument('--include_subworkflows', default=False)
+    args = parser.parse_args()
+
+    print('Using analysis output path: {0}'.format(args.analysis_output_path))
+
+    # Get the workflow id and metadata, write them to files
+    workflow_id = get_analysis_workflow_id(
+        analysis_output_path=args.analysis_output_path
+    )
+    get_metadata(
+        cromwell_url=args.cromwell_url,
+        workflow_id=workflow_id,
+        http_requests=HttpRequests(),
+        include_keys=args.include_keys,
+    )
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 
 setup(
-    name='pipeline-tools',
+    name='adapter_tools',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     classifiers=['Programming Language :: Python :: 3'],
     description='Utilities for retrieving files from the HCA data storage service and submitting an analysis bundle '
     'to HCA-DCP',
-    url='http://github.com/HumanCellAtlas/skylab',
+    url='http://github.com/broadinstitute/hca-adapter_tools',
     author='Human Cell Atlas Data Coordination Platform Mint Team',
     author_email='mintteam@broadinstitute.org',
     license='BSD 3-clause "New" or "Revised" License',
-    packages=['pipeline_tools'],
+    packages=['adapter_tools'],
     install_requires=[
         'arrow>=0.12.1',
         'google-auth>=1.6.1,<2',
@@ -23,27 +23,23 @@ setup(
         'requests-mock>=1.5.2,<2',
         'setuptools_scm>=2.0.0,<3',
         'tenacity>=5.0.2,<5.1',
-        'PyJWT==1.6.4',
-        'hca-metadata-api @ git+git://github.com/HumanCellAtlas/metadata-api@release/1.0b20',
+        'PyJWT==1.6.4'
     ],
     entry_points={
         'console_scripts': [
-            'get-analysis-workflow-metadata=pipeline_tools.shared.submission.get_analysis_workflow_metadata:main',
-            'create-envelope=pipeline_tools.shared.submission.create_envelope:main',
-            'create-file-descriptor=pipeline_tools.shared.submission.create_file_descriptor:main',
-            'create-analysis-file=pipeline_tools.shared.submission.create_analysis_file:main',
-            'create-analysis-protocol=pipeline_tools.shared.submission.create_analysis_protocol:main',
-            'create-analysis-process=pipeline_tools.shared.submission.create_analysis_process:main',
-            'create-links=pipeline_tools.shared.submission.create_links:main',
-            'create-reference-file=pipeline_tools.shared.submission.create_reference_file:main',
-            'get-upload-urn=pipeline_tools.shared.submission.get_upload_urn:main',
-            'get-bucket-date=pipeline_tools.shared.submission.get_bucket_date:main',
-            'get-files-to-upload=pipeline_tools.shared.submission.get_files_to_upload:main',
-            'confirm-submission=pipeline_tools.shared.submission.confirm_submission:main',
-            'parse-metadata=pipeline_tools.shared.submission.parse_cromwell_metadata:main',
-            'get-reference-file-details=pipeline_tools.shared.submission.get_reference_details:main',
-            'get-process-input-ids=pipeline_tools.shared.submission.get_process_input_ids:main',
-            'copy-adapter-outputs=pipeline_tools.shared.submission.copy_adapter_outputs:main'
+            'copy-adapter-outputs=adapter_tools.copy_adapter_outputs:main'
+            'create-analysis-file=adapter_tools.commands.create_analysis_file:main',
+            'create-analysis-process=adapter_tools.commands.create_analysis_process:main',
+            'create-analysis-protocol=adapter_tools.commands.create_analysis_protocol:main',
+            'create-file-descriptor=adapter_tools.commands.create_file_descriptor:main',
+            'create-links=adapter_tools.commands.create_links:main',
+            'create-reference-file=adapter_tools.commands.create_reference_file:main',
+            'get-analysis-workflow-metadata=adapter_tools.commands.get_analysis_workflow_metadata:main',
+            'get-bucket-date=adapter_tools.commands.get_bucket_date:main',
+            'get-process-input-ids=adapter_tools.commands.get_process_input_ids:main',
+            'get-reference-file-details=adapter_tools.commands.get_reference_file_details:main',
+            'merge-looms=adapter_tools.commands.merge_looms:main'
+            'parse-metadata=adapter_tools.commands.parse_metadata:main',
         ]
     },
     include_package_data=True,
