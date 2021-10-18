5e36c401431ce790a47793214d2896efa01ca5a3
Wes Dingman <wdingman@broadinstitute.org>
2021-10-18 14:17:03 -0400

    Refactor Dockerfile and remove unused packages (#3)
    * Move command line utils from pipeline-tools to adapter-tools
    
    * Get rid of pipeline-tools
    
    * Fix README
    
    * Add container check to pullaprove
    
    * Clean dockerfile and remove unused packages from adapter_tools

--- setup.py
+++ setup.py
@@ -6,18 +6,14 @@ setup(
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     classifiers=['Programming Language :: Python :: 3'],
-    description='Utilities for retrieving files from the HCA data storage service and submitting an analysis bundle '
-    'to HCA-DCP',
+    description='Command-line utilities for Human Cell Atlas adapter pipelines',
     url='http://github.com/broadinstitute/hca-adapter_tools',
-    author='Human Cell Atlas Data Coordination Platform Mint Team',
-    author_email='mintteam@broadinstitute.org',
+    author='Human Cell Atlas Data Coordination Platform - Lantern Team',
+    author_email='Broad Institute DSDE <dsde-engineering@broadinstitute.org>',
     license='BSD 3-clause "New" or "Revised" License',
     packages=['adapter_tools'],
     install_requires=[
         'arrow>=0.12.1',
-        'google-auth>=1.6.1,<2',
-        'google-cloud-storage>=1.10.0,<2',
-        'hca>=7.0.0,<8',
         'mock>=2.0.0,<3',
         'requests>=2.20.0,<3',
         'requests-mock>=1.5.2,<2',
@@ -27,7 +23,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'copy-adapter-outputs=adapter_tools.copy_adapter_outputs:main'
+            'copy-adapter-outputs=adapter_tools.commands.copy_adapter_outputs:main',
             'create-analysis-file=adapter_tools.commands.create_analysis_file:main',
             'create-analysis-process=adapter_tools.commands.create_analysis_process:main',
             'create-analysis-protocol=adapter_tools.commands.create_analysis_protocol:main',
@@ -38,7 +34,7 @@ setup(
             'get-bucket-date=adapter_tools.commands.get_bucket_date:main',
             'get-process-input-ids=adapter_tools.commands.get_process_input_ids:main',
             'get-reference-file-details=adapter_tools.commands.get_reference_file_details:main',
-            'merge-looms=adapter_tools.commands.merge_looms:main'
+            'merge-looms=adapter_tools.commands.merge_looms:main',
             'parse-metadata=adapter_tools.commands.parse_metadata:main',
         ]
     },
