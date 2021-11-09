5da27a9a041a1e0f86bd8da1506a4c8978162331
Wes Dingman <wdingman@asymmetrik.com>
2021-11-09 09:23:39 -0500

    Fixing precommit

--- adapter_tools/commands/validate_staging_area.py
+++ adapter_tools/commands/validate_staging_area.py
@@ -2,8 +2,7 @@
 Runs a pre-check of a staging area to identify issues that might cause the
 snapshot or indexing processes to fail.
 
-Code ported from
-https://github.com/DataBiosphere/hca-import-validation
+Code ported from https://github.com/DataBiosphere/hca-import-validation
 """
 import argparse
 import sys
